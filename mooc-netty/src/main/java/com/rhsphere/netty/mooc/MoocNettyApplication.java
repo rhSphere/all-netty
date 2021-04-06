5be3ef23c36c14b192722d8658419e66f1c26e81
rhsphere <rhsphere@gmail.com>
2021-04-06 21:21:41 +0800

    新增mooc-netty模块

---
+++ mooc-netty/src/main/java/com/rhsphere/netty/mooc/MoocNettyApplication.java
@@ -0,0 +1,13 @@
+package com.rhsphere.netty.mooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoocNettyApplication.class, args);
+	}
+
+}
---
+++ mooc-netty/src/test/java/com/rhsphere/netty/mooc/MoocNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhsphere.netty.mooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoocNettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
