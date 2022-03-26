6e717fb191fd03e1c87f2bbc45408ead293fc81a
rhsphere <rhsphere@gmail.com>
2022-03-26 10:23:41 +0800

    Server和Client

---
+++ architect-netty/src/main/java/com/rhsphere/netty/architect/pkg1/Client.java
@@ -0,0 +1,42 @@
+package com.rhsphere.netty.architect.pkg1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @author ludepeng
+ * @date 2022-03-26 10
+ */
+
+public class Client {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+
+        bootstrap.group(group)
+            .channel(NioSocketChannel.class)
+            .handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new FixedLengthFrameDecoder(5));
+                    ch.pipeline().addLast(new StringDecoder());
+                    ch.pipeline().addLast(new ClientHandler());
+                }
+            });
+
+        ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 8725).sync();
+        channelFuture.channel().writeAndFlush(Unpooled.wrappedBuffer("aaaaabbbbb".getBytes()));
+        Thread.sleep(2000);
+        channelFuture.channel().writeAndFlush(Unpooled.wrappedBuffer("cccccbbb".getBytes()));
+        channelFuture.channel().closeFuture().sync();
+        group.shutdownGracefully();
+    }
+}
---
+++ architect-netty/src/main/java/com/rhsphere/netty/architect/pkg1/ServerHandler.java
@@ -0,0 +1,34 @@
+package com.rhsphere.netty.architect.pkg1;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author ludepeng
+ * @date 2022-03-26 10
+ */
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(" server channel active... ");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String request = (String) msg;
+        System.out.println("Server :" + msg);
+        String response = request;
+        ctx.writeAndFlush(Unpooled.copiedBuffer(response.getBytes()));
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable t) throws Exception {
+
+    }
+}
